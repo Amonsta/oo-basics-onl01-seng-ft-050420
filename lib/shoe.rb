f1202dad6d94267aeae9524a317713a831e12cda
Amana Ammishaddai <amana.ammi@gmail.com>
2020-05-15 19:16:04 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,27 @@
 # Make your shoe class here!
+
+class Shoe
+
+  attr_accessor :color, :size, :material
+  attr_reader :brand
+  
+  def initialize(brand)
+    @brand = brand
+  end
+  
+
+  def cobble
+    puts @repaired = "Your shoe is as good as new!"
+  end 
+  
+   def condition=(condition)
+    @condition = condition
+  end 
+  
+   def condition
+    @condition == @repaired ? "new" : condition
+    
+  end 
+  
+  
+end
